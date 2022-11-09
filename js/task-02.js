b9e9e67d0ca7497a44388044581204003f1e5825
Maxym Koralkov <maxyma.sanx@gmail.com>
2022-11-09 15:38:00 +0200

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+function createIngredients(ingredients) {
+  return ingredients.map((item) => {
+    const ingredientsItem = document.createElement("li");
+    ingredientsItem.textContent = item;
+    ingredientsItem.classList.add("item");
+    return ingredientsItem;
+  });
+}
+ingredientsList.append(...createIngredients(ingredients));
