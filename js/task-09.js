494f4ad235376c366ce9b4ced7e24a8d40eaf813
Maxym Koralkov <maxyma.sanx@gmail.com>
2022-11-10 17:19:19 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const refs = {
+  body: document.querySelector("body"),
+  span: document.querySelector(".color"),
+  button: document.querySelector(".change-color"),
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function onChangeColorButtonClick() {
+  console.log(refs.body.style.color);
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.span.textContent = getRandomHexColor();
+}
+
+refs.button.addEventListener("click", onChangeColorButtonClick);
