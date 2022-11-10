be3354e0196aa457cb5fe3a25a503158a414f312
Maxym Koralkov <maxyma.sanx@gmail.com>
2022-11-10 18:13:18 +0200

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,38 @@
+const refs = {
+  input: document.querySelector("#controls input"),
+  create: document.querySelector("[data-create]"),
+  destroy: document.querySelector("[data-destroy]"),
+  boxes: document.querySelector("#boxes"),
+};
+
+refs.input.value = refs.input.min;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function createBoxes(amount) {
+  amount = refs.input.value;
+
+  const boxesArr = [];
+
+  for (let i = 0, k = 30; i < amount; i += 1, k += 10) {
+    boxesArr.push(
+      `<div style="background-color: ${getRandomHexColor()}; width: ${k}px; height: ${k}px"></div>`
+    );
+  }
+
+  refs.boxes.insertAdjacentHTML("beforeend", boxesArr.join(""));
+}
+
+function destroyBoxes() {
+  refs.boxes.innerHTML = "";
+  refs.input.value = refs.input.min;
+}
+
+refs.create.addEventListener("click", () => {
+  refs.boxes.innerHTML = "";
+  createBoxes();
+});
+
+refs.destroy.addEventListener("click", destroyBoxes);
