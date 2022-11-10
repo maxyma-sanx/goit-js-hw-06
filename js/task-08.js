d0accb9652c1b1aa5ff3786fa0be5a3520ef6294
Maxym Koralkov <maxyma.sanx@gmail.com>
2022-11-10 17:07:14 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  email.value && password.value
+    ? console.log({
+        [email.name]: email.value,
+        [password.name]: password.value,
+      })
+    : alert("Все поля должны быть заполнены");
+
+  event.currentTarget.reset();
+}
+
+form.addEventListener("submit", onFormSubmit);
